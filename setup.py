c89202e84398c81a177f1175827fcb1a4edba30e
Davide Nunes <davidelnunes@gmail.com>
2019-03-15 01:06:59 +0000

    added travis deploy

--- setup.py
+++ setup.py
@@ -1,14 +1,73 @@
-from distutils.core import setup
+#!/usr/bin/env python
+import os
+from setuptools import find_packages, setup, Command
+import codecs
+import sys
+from shutil import rmtree
 
-_VERSION = '0.6'
+__VERSION__ = '0.7'
+about = {}
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+with open(os.path.join(here, "exp", "__version__.py")) as f:
+    exec(f.read(), about)
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as 
+f:
+    long_description = "\n" + f.read()
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist bdist_wheel upload")
+    sys.exit()
+
+class UploadCommand(Command):
+    """Support setup.py publish."""
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
+        except FileNotFoundError:
+            pass
+        self.status("Building Source distribution…")
+        os.system("{0} setup.py sdist bdist_wheel".format(sys.executable))
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload dist/*")
+        self.status("Pushing git tags…")
+        os.system("git tag v{0}".format(about["__version__"]))
+        os.system("git push --tags")
+        sys.exit()
+
+
 
 install_requires = [
-    'tensorflow >= 1.13.0'
+    
 ]
+
+extras_require = {
+	'graphviz' : ['pygraphviz']
+}
 
 setup(
     name='tensorx',
-    version=_VERSION,
+    version=about["__version__"],
     packages=['tensorx'],
     url='https://github.com/davidenunes/tensorx',
     license='Apache 2.0',
@@ -16,7 +75,16 @@ setup(
     author_email='mail@davidenunes.com',
     description='TensorX is a minimalistic utility library to '
                 'build neural network models in TensorFlow',
-
-    python_requires='>=3.7',
+    package_data={
+          "": ["LICENSE"],
+    },
+    include_package_data=True,
+    classifiers=[
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+    ]
+    python_requires='>=3.6',
+    cmdclass={"upload": UploadCommand},
     install_requires=install_requires
 )
