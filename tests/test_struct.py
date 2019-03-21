46e562c78eca2ff6fffe67ecb108ffd5342d20a8
Davide Nunes <davidelnunes@gmail.com>
2019-03-21 21:48:57 +0000

    fixed tests for struct

--- tests/test_struct.py
+++ tests/test_struct.py
@@ -2,6 +2,7 @@ from tensorx import test_utils
 import tensorx as tx
 import tensorflow as tf
 import os
+from tensorx.struct import IndexValueTable
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
@@ -16,7 +17,7 @@ class TestStruct(test_utils.TestCase):
                     [0, 0, 1, -1, 0, 0],
                     [0, 0, 0, 0, 1, -1]]
 
-        table = tx.IndexValueTable.from_list(i, v, 6)
+        table = IndexValueTable.from_list(i, v, 6)
 
         sp_tensor = table.to_sparse_tensor()
         result = tf.sparse.to_dense(sp_tensor)
