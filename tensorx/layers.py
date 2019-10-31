started to fix eager computation on single layers refactoring compute / __call__

tensorx/layers.py
@@ -9,6 +9,7 @@ from tensorx.callbacks import OnValueChange
 from functools import partial
 from tensorx.math import embedding_lookup_sparse
 from tensorflow.python.training.tracking.tracking import AutoTrackable
+from tensorflow.python.training.tracking import data_structures as track
 
 
 class LayerState(AutoTrackable):
@@ -19,7 +20,10 @@ class LayerState(AutoTrackable):
         ideally ``LayerState`` objects would contain only ``Variable`` instances, but
         storing Layers can be more convenient.
     """
-    pass
+
+    def __init__(self, origin_layer):
+        # don't repeat tracking on layer
+        self.layer = track.NoDependency(origin_layer)
 
     def variables(self):
         """ returns a list of all variables contained in the layer state object
@@ -41,7 +45,7 @@ class LayerState(AutoTrackable):
         """
         all_vars = dict()
         for attr, state in self.__dict__.items():
-            if isinstance(state, Layer):
+            if isinstance(state, Layer) and not state == self.layer:
                 v = state.layer_state.var_dict()
                 all_vars.update(v)
             elif isinstance(state, tf.Variable):
@@ -108,19 +112,19 @@ class LayerProto:
 
     def _validate_args(self, **kwargs):
         for key in kwargs:
-            if key not in self.args:
+            if key not in self.arg_spec:
                 raise TypeError("{} prototype got an unexpected argument {}".format(self.layer_cls.__name__, key))
 
     def __init__(self, layer_cls, **kwargs):
         self.layer_cls = layer_cls
         spec = inspect.getfullargspec(layer_cls.__init__)
-        self.args = set(spec.args[1:] + spec.kwonlyargs)
+        self.arg_spec = set(spec.args[1:] + spec.kwonlyargs)
         self._validate_args(**kwargs)
 
-        self.args_set = kwargs
+        self.arg_dict = kwargs
 
     def __call__(self, *args, **kwargs):
-        new_args = dict(self.args_set)
+        new_args = dict(self.arg_dict)
         new_args.update(kwargs)
         return self.layer_cls(*args, **new_args)
 
@@ -133,7 +137,7 @@ class LayerProto:
             **kwargs: new values for constructor named arguments to be updated
         """
         self._validate_args(**kwargs)
-        self.args_set.update(kwargs)
+        self.arg_dict.update(kwargs)
 
 
 class Layer(AutoTrackable):
@@ -223,13 +227,13 @@ class Layer(AutoTrackable):
             input_signature:
 
         Returns:
-
+            a function with the entire graph traced from the current layer as the output
         """
         # @tf.function(input_signature=(tf.TensorSpec(shape=[None], dtype=tf.int32),))
         return tf.function(self.compute, input_signature)
 
     def init_state(self):
-        self.layer_state = LayerState()
+        self.layer_state = LayerState(self)
         return self.layer_state
 
     def __str__(self):
@@ -1074,7 +1078,6 @@ class Linear(Layer):
         super().__init__(input_layers=input_layer, n_units=n_units, dtype=dtype, name=name)
 
     def init_state(self):
-        self.layer_state = super().init_state()
 
         with layer_scope(self):
             input_layer = self.input_layers[0]
@@ -1085,8 +1088,10 @@ class Linear(Layer):
                 if not isinstance(self.share_state_with, Linear):
                     raise TypeError("Layer can only share variables with other layer of the same type")
 
+                self.layer_state = self.share_state_with.layer_state
+
                 shape = [input_layer.n_units, self.n_units]
-                shared_shape = self.share_state_with.weights.get_shape().as_list()
+                shared_shape = self.layer_state.weights.get_shape().as_list()
                 if self.transpose_weights:
                     shared_shape = shared_shape[::-1]
 
@@ -1095,6 +1100,8 @@ class Linear(Layer):
                                      "share_state_with is provided but \n"
                                      "self shape: {s0} different from "
                                      "other shape: {s1}".format(s0=shape, s1=shared_shape))
+            else:
+                self.layer_state = super().init_state()
 
             # if weights are passed, check that their shape matches the layer shape
             if self.shared_weights is not None:
@@ -1122,7 +1129,7 @@ class Linear(Layer):
                         "invalid shared bias: number of bias {} does not match number of units {}".format(bias_shape[0],
                                                                                                           self.n_units))
 
-            weights = self.share_state_with.weights if self.share_state_with is not None else self.shared_weights
+            weights = getattr(self.layer_state, "weights", self.shared_weights)
             if weights is None:
                 init_value = self.weight_init(self.shape, dtype=self.dtype)
                 weights = tf.Variable(initial_value=init_value,
@@ -1130,16 +1137,17 @@ class Linear(Layer):
                                       dtype=self.dtype,
                                       name="weights")
 
-            self.layer_state.weights = weights
+            if not hasattr(self.layer_state, "weights"):
+                self.layer_state.weights = weights
 
-            bias = self.share_state_with.bias if self.share_state_with is not None else self.shared_bias
+            bias = getattr(self.layer_state, "bias", self.shared_bias)
             if self.add_bias:
                 if bias is None:
                     bias = tf.Variable(initial_value=self.bias_init([self.n_units], self.dtype),
                                        name="bias", trainable=True)
-            else:
-                bias = None
-            self.layer_state.bias = bias
+
+            if not hasattr(self.layer_state, "bias"):
+                self.layer_state.bias = bias
 
     def compute(self, *input_layers):
         if not input_layers:
@@ -1210,7 +1218,7 @@ class Linear(Layer):
                                        b_is_sparse=self.sparse_weights)
 
             # y = xW + [b]
-            if self.bias is not None:
+            if self.add_bias:
                 tensor = tf.nn.bias_add(tensor, self.bias, name="add_b")
 
         return tensor
@@ -1375,10 +1383,17 @@ class ViewLayer(Layer):
 class DropConnect(ViewLayer):
     """ DropConnect
 
+    Wraps around a Linear layer to create a new layer where connections between input and linear units
+    can be dropped with a given probability.
+
+    Note:
+        as opposed to ``Dropout`` which does not wrap a layer but rather drops the outputs of it's previous
+        layer.
+
     Args:
-            layer (Layer):
-            probability (float):
-            locked (bool):
+            layer (Linear): ``Linear`` layer to be wrapped in DropConnect
+            probability (float): probability of dropping a connection between units
+            locked (bool): if true
             name (str):
 
     """
@@ -1414,25 +1429,28 @@ class DropConnect(ViewLayer):
         input_layer = as_layer(input_layers[0])
 
         with layer_scope(self):
-            w = self.inner_layer.weights
-            b = self.inner_layer.bias
+            weights = self.inner_layer.weights
+            bias = self.inner_layer.bias
 
-            drop_w, w_mask = txf.dropout(w, probability=self.probability, random_mask=self.layer_state.weight_mask,
+            drop_w, w_mask = txf.dropout(weights, probability=self.probability,
+                                         random_mask=self.layer_state.weight_mask,
                                          scale=False,
                                          return_mask=True)
             # self.layer_state.weight_mask = w_mask
-            drop_b = None
-            add_bias = b is not None
+            drop_bias = None
+            add_bias = bias is not None
             if add_bias:
-                drop_b, b_mask = txf.dropout(b, probability=self.probability, random_mask=self.layer_state.bias_mask,
-                                             scale=False,
-                                             return_mask=True)
+                drop_bias, b_mask = txf.dropout(bias,
+                                                probability=self.probability,
+                                                random_mask=self.layer_state.bias_mask,
+                                                scale=False,
+                                                return_mask=True)
                 # self.layer_state.bias_mask = b_mask
 
             new_linear = Linear(input_layer,
                                 n_units=self.n_units,
                                 shared_weights=drop_w,
-                                shared_bias=drop_b,
+                                shared_bias=drop_bias,
                                 add_bias=add_bias)
             # forward weights and bias
             self.weights = new_linear.weights
@@ -1482,6 +1500,7 @@ class Dropout(Layer):
     def __init__(self, input_layer,
                  probability=0.1,
                  scale=True,
+                 mask=None,
                  noise_shape=None,
                  locked=False,
                  seed=None,
@@ -1490,8 +1509,9 @@ class Dropout(Layer):
         self.seed = seed
         self.probability = probability
         self.scale = scale
-        self.noise_shape = noise_shape
         self.locked = locked
+        self.mask = mask
+        self.noise_shape = noise_shape
         self.share_state_with = share_state_with
 
         super().__init__(input_layers=input_layer,
@@ -1500,21 +1520,40 @@ class Dropout(Layer):
                          name=name)
 
     def init_state(self):
-        if self.share_state_with is None:
-            layer_state = super().init_state()
-            layer_state.mask = None
-            with layer_scope(self):
-                with tf.name_scope(name="random_mask"):
-                    if self.noise_shape is not None:
-                        keep_prob = 1 - self.probability
-                        random_state = tf.random.uniform(self.noise_shape, seed=self.seed,
-                                                         dtype=self.input_layers[0].dtype)
-                        mask = keep_prob + random_state
-                        mask = tf.math.floor(mask, name="binary_mask")
+        input_layer = self.input_layers[0]
+        mask = as_layer(self.mask) if self.mask is not None else None
 
-                        layer_state.mask = mask
+        # random mask layer
+        @layer(n_units=input_layer.n_units, name="random_mask")
+        def random_mask(input_layer):
+            noise_shape = tf.shape(input_layer)
+            dtype = input_layer.dtype
+
+            keep_prob = 1 - self.probability
+            random_state = tf.random.uniform(noise_shape,
+                                             seed=self.seed,
+                                             dtype=dtype)
+            mask = keep_prob + random_state
+            mask = tf.math.floor(mask, name="binary_mask")
+            return mask
+
+        if self.share_state_with is None:
+            self.layer_state = super().init_state()
+            if mask is not None:
+                self.layer_state.mask = mask
+            else:
+                with layer_scope(self):
+                    if self.locked:
+                        self.layer_state.mask = random_mask(input_layer)
+                    else:
+                        self.layer_state.mask = None
         else:
             self.layer_state = self.share_state_with.layer_state
+
+        # TODO usually we don't manipulate input_layers directly, but it might be needed in this case
+        #  because the random mask is a new dependency on this layer
+        if hasattr(self.layer_state, "mask"):
+            self._input_layers.append(self.layer_state.mask)
 
         return self.layer_state
 
@@ -1523,12 +1562,18 @@ class Dropout(Layer):
             input_layers = self.input_layers
 
         input_value = as_layer(input_layers[0]).compute()
+        if len(input_layers) > 1:
+            mask = as_layer(input_layers[1])
+        else:
+            mask = self.layer_state.mask
+
+        mask_value = mask.compute()
 
         with layer_scope(self):
             if isinstance(input_value, tf.SparseTensor):
                 # if input is sparse, noise_shape is not used
                 tensor, mask = txf.sparse_dropout(sp_tensor=input_value,
-                                                  mask=self.layer_state.mask,
+                                                  mask=mask_value,
                                                   probability=self.probability,
                                                   scale=self.scale,
                                                   return_mask=True,
@@ -1537,23 +1582,22 @@ class Dropout(Layer):
             else:
                 tensor, mask = txf.dropout(tensor=input_value,
                                            noise_shape=self.noise_shape,
-                                           random_mask=self.layer_state.mask,
+                                           random_mask=mask_value,
                                            probability=self.probability,
                                            scale=self.scale,
                                            return_mask=True,
                                            seed=self.seed)
 
-            if self.layer_state.mask is None:
-                self.layer_state.mask = mask
-
             return tensor
 
-    def reuse_with(self, layer, name=None, locked=None):
+    def reuse_with(self, layer, mask=None, name=None, locked=None):
         locked = self.locked if locked is None else locked
         name = self.name if name is None else name
         share_state_with = self if locked else None
+        mask = self.mask if mask is None else mask
 
         return Dropout(layer,
+                       mask=mask,
                        probability=self.probability,
                        noise_shape=self.noise_shape,
                        scale=self.scale,
@@ -3023,5 +3067,6 @@ __all__ = [
     "RNN",
     "Lookup",
     "ToDense",
-    "ToSparse"
+    "ToSparse",
+    "Dropout"
 ]

tensorx/test_utils.py
@@ -3,22 +3,56 @@ import numpy as np
 from tensorx import Layer
 from typing import Any
 from numbers import Number
-from tensorflow import Tensor
+from tensorflow import Tensor, Variable
 
 
 class TestCase(unittest.TestCase):
     def assertArrayEqual(self, first, second):
         if isinstance(first, Layer):
-            first = first.tensor()
+            first = first()
         if isinstance(second, Layer):
-            second = second.tensor()
+            second = second()
 
         self.assertTrue(np.array_equal(first, second))
 
     def assertArrayNotEqual(self, first, second):
         if isinstance(first, Layer):
-            first = first.tensor()
+            first = first()
         if isinstance(second, Layer):
-            second = second.tensor()
+            second = second()
 
         self.assertFalse(np.array_equal(first, second))
+
+    def assertAlmostEqual(self, first, second, places=None, msg=None,
+                          delta=None):
+        if isinstance(first, (Layer)):
+            first = first()
+        if isinstance(second, (Layer)):
+            second = second()
+
+        super().assertAlmostEqual(first, second, places, msg, delta)
+
+    def assertAllClose(self, actual, desired, rtol=1e-7, atol=0., verbose=True):
+        """ It compares the difference between actual and desired to
+        ``atol + rtol * abs(desired)``.
+
+        if actual or desired are Tensors, tries to evaluate them, so this
+        should be called inside a ``with self.cashed_session():``
+
+        Args:
+            actual: array obtained
+            desired: desired array value
+            rtol: relative tolerance
+            atol: absolute tolerance
+            verbose: if True, the conflicting values are appended to the error message
+
+        Raises:
+            AssertionError: If actual and desired are not equal up to specified precision.
+
+        """
+        if isinstance(actual, Layer):
+            actual = actual()
+        if isinstance(desired, Layer):
+            desired = desired()
+
+        np.testing.assert_allclose(actual, desired, rtol=rtol, atol=atol, verbose=verbose)

tensorx/transform.py
@@ -310,7 +310,7 @@ def sparse_dropout(sp_tensor,
         name: A name for this operation (optional).
 
     """
-    with tf.name_scope(name=name, values=[sp_tensor]):
+    with tf.name_scope(name=name):
         dense_shape = sp_tensor.dense_shape
 
         if not sp_tensor.values.dtype.is_floating:

tests/test_autograph.py
@@ -159,15 +159,7 @@ class TestAutoGraph(unittest.TestCase):
             x1.value = in0
             return y2.compute()
 
-        @tf.function
-        def simple_graph2(in0):
-            y_2 = y1.compute(in0)
-            # y_1 = y10.compute(in0)
-            # y_2 = y11.compute(y_1)
-            # if we call y1.compute(in0) it calls reuse with on the input which creates more tensors
-            #   this is inefficient perhaps we can compile the module and use this function
-            # DONE
-            return y2.compute(y_2, tf.ones([1, 3]))
+        simple_graph_2 = Graph.build(inputs=[x1, x2], outputs=y2)
 
         g = Graph.build(inputs=[x1, x2], outputs=y2)
 
@@ -195,7 +187,7 @@ class TestAutoGraph(unittest.TestCase):
         t1 = timeit(update_run, number=n)
         t2 = timeit(lambda: compiled_fn(tf.random.uniform([256, 1000])), number=n)
         t3 = timeit(lambda: simple_graph(tf.random.uniform([256, 1000])), number=n)
-        t4 = timeit(lambda: simple_graph2(tf.random.uniform([256, 1000])), number=n)
+        t4 = timeit(lambda: simple_graph_2(tf.random.uniform([256, 1000])), number=n)
         # t5 = timeit(lambda: compiled_recursive(tf.random.uniform([256, 1000])), number=n)
 
         print(f"{t1}\tupdate input and run")

tests/test_layers.py
@@ -642,108 +642,142 @@ class TestLayers(TestCase):
         # self.assertEqual(np.shape(c1)[-1], m * 2)
         # self.assertEqual(np.shape(c2)[-1], m * 3)
 
+    def test_lookup_sequence_sparse(self):
+        input_dim = 10
+        embed_dim = 3
+        seq_size = 2
+        batch_size = 3
 
-def test_lookup_sequence_sparse(self):
-    input_dim = 10
-    embed_dim = 3
-    seq_size = 2
-    batch_size = 3
+        sparse_input = tf.SparseTensor([[0, 2], [1, 0], [2, 1]], [1, 1, 1], [3, input_dim])
+        sparse_input_1d = tf.SparseTensor([[2], [0], [1]], [1, 1, 1], [input_dim])
+        tensor_input = tx.Tensor(sparse_input, input_dim)
+        tensor_input_1d = tx.Tensor(sparse_input_1d, input_dim)
 
-    sparse_input = tf.SparseTensor([[0, 2], [1, 0], [2, 1]], [1, 1, 1], [3, input_dim])
-    sparse_input_1d = tf.SparseTensor([[2], [0], [1]], [1, 1, 1], [input_dim])
-    tensor_input = TensorLayer(sparse_input, input_dim)
-    tensor_input_1d = TensorLayer(sparse_input_1d, input_dim)
+        lookup = tx.Lookup(tensor_input, seq_size,
+                           embedding_shape=[input_dim, embed_dim],
+                           batch_size=batch_size,
+                           batch_padding=False)
 
-    lookup = Lookup(tensor_input, seq_size,
-                    lookup_shape=[input_dim, embed_dim],
-                    batch_size=batch_size,
-                    batch_padding=False)
+        lookup_padding = tx.Lookup(tensor_input, seq_size,
+                                   embedding_shape=[input_dim, embed_dim],
+                                   batch_size=batch_size,
+                                   batch_padding=True)
 
-    lookup_padding = Lookup(tensor_input, seq_size,
-                            lookup_shape=[input_dim, embed_dim],
-                            batch_size=batch_size,
-                            batch_padding=True)
+        lookup_1d = tx.Lookup(tensor_input_1d, seq_size,
+                              embedding_shape=[input_dim, embed_dim],
+                              batch_size=batch_size,
+                              batch_padding=True)
 
-    lookup_1d = Lookup(tensor_input_1d, seq_size,
-                       lookup_shape=[input_dim, embed_dim],
-                       batch_size=batch_size,
-                       batch_padding=True)
-
-    init = tf.global_variables_initializer()
-    with self.cached_session(use_gpu=True):
-        self.eval(init)
-
-        result = self.eval(lookup.tensor)
-        result_padding = self.eval(lookup_padding.tensor)
-        result_1d = self.eval(lookup_1d.tensor)
+        result = lookup()
+        result_padding = lookup_padding()
+        result_1d = lookup_1d()
 
         self.assertEqual(np.shape(result), (2, seq_size, embed_dim))
         self.assertEqual(np.shape(result_padding), (batch_size, seq_size, embed_dim))
         self.assertEqual(np.shape(result_1d), (batch_size, seq_size, embed_dim))
 
+    def test_lookup_sparse_padding(self):
+        input_dim = 6
+        embed_dim = 3
+        seq_size = 1
 
-def test_lookup_sparse_padding(self):
-    input_dim = 6
-    embed_dim = 3
-    seq_size = 1
+        sparse_input = tf.SparseTensor([[0, 1], [0, 3], [1, 0]], [1, 1, 1], [2, input_dim])
+        sparse_input = tx.Tensor(sparse_input, input_dim)
 
-    sparse_input = tf.SparseTensor([[0, 1], [0, 3], [1, 0]], [1, 1, 1], [2, input_dim])
-    sparse_input = TensorLayer(sparse_input, input_dim)
+        lookup = tx.Lookup(sparse_input,
+                           seq_size=seq_size,
+                           embedding_shape=[input_dim, embed_dim],
+                           batch_size=None,
+                           batch_padding=False)
 
-    lookup = Lookup(sparse_input,
-                    seq_size=seq_size,
-                    lookup_shape=[input_dim, embed_dim],
-                    batch_size=None,
-                    batch_padding=False)
+        result = lookup()
 
-    init = tf.global_variables_initializer()
-    with self.cached_session(use_gpu=True):
-        self.eval(init)
-        self.eval(lookup.tensor)
+    def test_lookup_sequence_bias(self):
+        vocab_size = 4
+        n_features = 3
+        seq_size = 2
 
+        inputs = tx.Input(n_units=seq_size, dtype=tf.int32)
+        input_data = np.array([[2, 0], [1, 2], [0, 2]])
+        lookup = tx.Lookup(input_layer=inputs,
+                           seq_size=seq_size,
+                           embedding_shape=[vocab_size, n_features],
+                           add_bias=True)
 
-def test_lookup_sequence_bias(self):
-    vocab_size = 4
-    n_features = 3
-    seq_size = 2
-
-    inputs = Input(seq_size, dtype=tf.int32)
-    input_data = np.array([[2, 0], [1, 2], [0, 2]])
-    lookup = Lookup(inputs, seq_size, lookup_shape=[vocab_size, n_features], bias=True)
-
-    init = tf.global_variables_initializer()
-    with self.cached_session(use_gpu=True):
-        self.eval(init)
-        v1 = self.eval(lookup.tensor, {inputs.placeholder: input_data})
+        inputs.value = input_data
+        v1 = lookup()
         self.assertEqual(np.shape(v1), (np.shape(input_data)[0], seq_size, n_features))
 
+    def test_lookup_sequence_transform(self):
+        vocab_size = 4
+        embed_dim = 2
+        seq_size = 2
 
-def test_lookup_sequence_transform(self):
-    vocab_size = 4
-    embed_dim = 2
-    seq_size = 2
+        inputs = tx.Input(n_units=seq_size, dtype=tf.int32)
+        input_data = np.array([[2, 0], [1, 2], [0, 2]])
+        lookup = tx.Lookup(inputs,
+                           seq_size=seq_size,
+                           embedding_shape=[vocab_size, embed_dim],
+                           add_bias=True)
+        concat_lookup = lookup.as_concat()
+        seq_lookup = lookup.permute_batch_time()
 
-    inputs = Input(seq_size, dtype=tf.int32)
-    input_data = np.array([[2, 0], [1, 2], [0, 2]])
-    lookup = Lookup(inputs, seq_size, lookup_shape=[vocab_size, embed_dim], bias=True)
-    concat_lookup = lookup.as_concat()
-    seq_lookup = lookup.permute_batch_time()
+        self.assertTrue(hasattr(lookup, "seq_size"))
 
-    self.assertTrue(hasattr(lookup, "seq_size"))
+        inputs.value = input_data
 
-    init = tf.global_variables_initializer()
-    with self.cached_session(use_gpu=True):
-        self.eval(init)
-
-        v1 = self.eval(lookup.tensor, {inputs.placeholder: input_data})
-        v2 = self.eval(concat_lookup.tensor, {inputs.placeholder: input_data})
-        v3 = self.eval(seq_lookup.tensor, {inputs.placeholder: input_data})
+        v1 = lookup()
+        v2 = concat_lookup()
+        v3 = seq_lookup()
 
         self.assertEqual(np.shape(v1), (np.shape(input_data)[0], seq_size, embed_dim))
         self.assertEqual(np.shape(v2), (np.shape(input_data)[0], seq_size * embed_dim))
 
         self.assertEqual(np.shape(v3), (seq_size, np.shape(input_data)[0], embed_dim))
         self.assertTrue(np.array_equal(v1[:, 0], v3[0]))
+
+    def test_reuse_dropout(self):
+        x1 = tx.Tensor(np.ones(shape=[2, 4]), dtype=tf.float32)
+
+        x2 = tx.Activation(x1)
+
+        drop1 = tx.Dropout(x2, probability=0.5, locked=True)
+
+        self.assertEqual(len(drop1.input_layers), 2)
+        self.assertIs(drop1.input_layers[0], x2)
+        self.assertIs(drop1.input_layers[-1], drop1.layer_state.mask)
+
+        # shared state overrides mask?
+        _, mask = tx.dropout(x2, return_mask=True)
+        drop2 = drop1.reuse_with(x2, mask)
+
+        self.assertEqual(len(drop2.input_layers), 2)
+        self.assertIs(drop2.input_layers[0], x2)
+        self.assertIs(drop2.input_layers[-1], drop2.layer_state.mask)
+
+        self.assertArrayNotEqual(drop1(), drop2())
+
+        graph = tx.Graph.build(inputs=None, outputs=[drop1, drop2])
+
+        print(list(graph.dependency_iter()))
+        #graph = graph.compile()
+
+        out1, out2 = graph()
+        print(out1)
+        print(out2)
+
+        # drop2 = tx.Dropout(x2, probability=0.5)
+        # drop3 = drop1.reuse_with(x1)
+        #
+        # d1, d2, d3 = drop1(),drop2(),drop3()
+        #
+        # self.assertIs(drop1.mask, drop3.mask)
+        # print(drop1.layer_state.mask)
+        # print(drop2.layer_state.mask)
+        # self.assertIsNot(drop1.mask, drop2.mask)
+        #
+        # self.assertArrayEqual(d1, d3)
+        # self.assertArrayNotEqual(d1, d2)
 
 
 if __name__ == '__main__':

tests/test_serialization.py
@@ -7,6 +7,8 @@ import unittest
 import tensorx as tx
 import tensorflow as tf
 from tensorflow.python.util import object_identity
+from tensorflow.python.training.tracking.tracking import AutoTrackable
+from tensorflow.python.training.tracking import util
 
 # checkpoint manager expexts trackable
 from tensorflow.python.training.tracking import base as trackable
@@ -27,8 +29,11 @@ class MyTestCase(TestCase):
 
         self.assertIsNot(w1, w2)
 
-        print(w1)
-        print(w2)
+        # print(w1)
+        # print(w2)
+
+        track: AutoTrackable = l1.layer_state
+        print(util.list_objects(track))
 
         ckpt = tf.train.Checkpoint(l1=l1)
         manager = tf.train.CheckpointManager(ckpt, './ckpts', max_to_keep=1)
@@ -36,15 +41,10 @@ class MyTestCase(TestCase):
         # manager.save(2)
 
         l1.weights.assign(l2.weights.value())
-        print(w1)
-        print(w2)
 
         status = ckpt.restore(manager.latest_checkpoint)
 
-        print(w1)
-        print(w2)
-
-        #print(status.assert_existing_objects_matched())
+        # print(status.assert_existing_objects_matched())
         print(tf.train.list_variables(manager.latest_checkpoint))
 
         # d = dict({w1: 0, w2: 1})