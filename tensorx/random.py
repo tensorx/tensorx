7b61b0f391eabebb6e50288476b6a4a2ded2b914
Davide Nunes <davidelnunes@gmail.com>
2020-08-21 01:44:31 +0100

    started adding random ops from tensorx v1 with tests

--- tensorx/random.py
+++ tensorx/random.py
@@ -1,4 +1,7 @@
 import tensorflow as tf
+from typing import Optional, List
+from tensorx.utils import as_tensor
+from tensorx.math import logit
 
 
 def gumbel_top(logits, num_samples, dtype=tf.int32, seed=None):
@@ -29,4 +32,72 @@ def gumbel_top(logits, num_samples, dtype=tf.int32, seed=None):
         return indices
 
 
-__all__ = ["gumbel_top"]
+def bernoulli(shape, prob=0.5, seed=None, dtype=tf.float32, name="random_bernoulli"):
+    """ Random bernoulli tensor.
+
+    A bernoulli tensor sampled from a random uniform distribution.
+
+    !!! info
+        The Bernoulli distribution takes the value _1_ with probability p and the value 0 with probability _q = 1 − p_
+
+    Args:
+        shape (`List[int]`): output tensor shape
+        prob (`float`): probability of each element being 1
+        seed (`int`): A Python `int`. Used in combination with `tf.random.set_seed` to create a reproducible sequence of
+            tensors across multiple calls.
+        dtype (`DType`): output tensor type
+        name (`Optional[str]`): name for random_bernoulli op
+
+    Returns:
+        tensor (`Tensor`): tensor with the given input shape
+    """
+    with tf.name_scope(name=name):
+        # uniform [prob, 1.0 + prob)
+        random_tensor = prob
+        random_tensor += tf.random.uniform(
+            shape, seed=seed, dtype=dtype)
+        return tf.math.floor(random_tensor)
+
+
+def sample_sigmoid(logits, n, dtype=None, seed=None, name="sample_sigmoid"):
+    """ sample_sigmoid
+
+    Efficient sampling Bernoulli random variable from a sigmoid defined distribution
+
+    !!! info
+        This can be applied to the output layer of a neural net if this represents a bernoulli
+        distribution defined using a parameterized sigmoid-activated layer
+
+    Args:
+        logits (`Tensor`): logits
+        n (`int`): number of samples per row of logits
+        dtype (`tf.DType`): input Tensor dtype
+        seed (`int`): random number generator seed
+        name (`Optional[str]`): name for sample sigmoid op (optional)
+
+    Returns:
+        samples (`Tensor`): a tensor with samples
+    """
+    with tf.name_scope(name):
+        logits = as_tensor(logits)
+        if dtype is not None:
+            logits = tf.cast(logits, dtype)
+        n = as_tensor(n)
+
+        shape = tf.shape(logits)
+        sample_shape = tf.concat([[n], shape], axis=-1)
+
+        uniform_sample = tf.random.uniform(sample_shape,
+                                           minval=0,
+                                           maxval=1,
+                                           dtype=logits.dtype, seed=seed)
+        z = logit(uniform_sample, dtype=logits.dtype)
+
+        return tf.cast(tf.greater(logits, z), tf.float32)
+
+
+__all__ = [
+    "gumbel_top",
+    "bernoulli",
+    "sample_sigmoid"
+]
--- tests/test_random.py
+++ tests/test_random.py
@@ -4,6 +4,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 import tensorx as tx
 import tensorflow as tf
+import pytest
 
 
 def test_gumbel_sample():
@@ -14,3 +15,31 @@ def test_gumbel_sample():
     all_unique = tf.reduce_all(all_unique, axis=0)
     assert all_unique
     assert sample.shape == [4, 2]
+
+
+def test_random_bernoulli():
+    prob = 0.2
+    binary = tx.random.bernoulli(shape=[10, 1000], prob=prob)
+    ones_prob = tf.reduce_mean(binary, axis=-1)
+    ones_expected = tf.constant(prob, shape=tf.shape(ones_prob))
+
+    assert tx.tensor_all_close(ones_prob,
+                               ones_expected,
+                               atol=4e-2)
+
+
+def test_sample_sigmoid():
+    shape = [4, 4]
+    n_samples = 2
+    # get some random logits
+    logits = tf.random.uniform(shape)
+    p = tf.nn.sigmoid(logits)
+
+    # expensive to sample
+    # u ~ Uniform(0,1)
+    # Y = sigmoid(x) > u
+    # equivalent to fast x > logit(u).
+
+    sample = tx.sample_sigmoid(logits, n_samples)
+
+    assert tx.tensor_equal(tf.shape(sample), [n_samples] + shape)
